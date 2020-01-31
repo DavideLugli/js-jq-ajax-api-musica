810942dfff4dd9078b4e9b0178a02534dbe208e1
Davide Lugli <luglidavide.web@gmail.com>
2020-01-31 16:29:04 +0100

    fine (no bonus)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
-	//Code
+
+  $.ajax({
+    'url': 'https://flynn.boolean.careers/exercises/api/array/music',
+    'method': 'GET',
+    'success': function(data) {
+      printAlbum(data.response);
+
+
+    },
+    'error': function(request, state, errors) {
+      alert('Errore ' + errors);
+    }
+  });
+
+
+
+function printAlbum(album) {
+for (var i = 0; i<album.length; i++) {
+var cd = album[i];
+console.log(cd);
+  // script x handlebars
+var source = $("#entry-template").html();
+var template = Handlebars.compile(source);
+
+var html = template(cd);
+$('.cds-container').append(html);
+}
+}
 });
